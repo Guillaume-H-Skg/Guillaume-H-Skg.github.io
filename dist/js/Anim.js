99384cc080921058b2e6f0437dd89065fc20afb5
Guillaume-H-Skg <91275635+Guillaume-H-Skg@users.noreply.github.com>
2023-03-08 03:06:27 +0100

    Anim Test

---
+++ dist/js/Anim.js
@@ -0,0 +1,42 @@
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry) =>{
+        if (entry.isIntersecting) {
+            entry.target.classList.add('play');
+        } else {
+            entry.target.classList.remove('play')
+        }
+    }); 
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=> observer.observe(el));
+
+
+
+// window.onload = () => {
+//     const anchors = document.querySelectorAll('a');
+//     const transition_el = document.querySelector('.transition');
+
+//     setTimeout(() => {
+//       transition_el.classList.remove('is-active');
+//     }, 500);
+  
+//     for (let i = 0; i < anchors.length; i++) {
+//       const anchor = anchors[i];
+  
+//       anchor.addEventListener('click', e => {
+//         e.preventDefault();
+//         let target = e.target.href;
+  
+//         console.log(transition_el);
+  
+//         transition_el.classList.add('is-active');
+  
+//         console.log(transition_el);
+  
+//         setInterval(() => {
+//           window.location.href = target;
+//         }, 500);
+//       })
+//     }
+//   }
